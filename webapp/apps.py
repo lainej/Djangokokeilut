dd55a10c354711b8e0104cea90571254489e71f5
Joona <joona-laine@hotmail.com>
2016-03-19 17:06:30 +0200

    No tämmöinen vielä

--- webapp/apps.py
+++ webapp/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class WebappConfig(AppConfig):
     name = 'webapp tästä poistetaan paskaa paljon, ettei tuu ongelmia!!'
+    paska = 'ja vittu mitä tämäkin tässä'
